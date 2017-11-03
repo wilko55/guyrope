b57567a08dfe36c555378d4496fe5b9204e1b37b
Mentor Match <mentormatch@Andys-Macbook.local>
2017-11-03 08:52:20 +0100

    Fixed merge conflicts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/campsites');
@@ -11,56 +12,6 @@ db.once('open', function() {
 });
 
 const Campsite = require('./data/campsite');
-
-let json = {
-  country: 'France',
-  region: 'Alsace',
-  name: 'Camping Palmer Aire naturelle',
-  type: 'Aire naturelle',
-  facilities: ['Showers', 'Toilets', 'BBQ', 'Electricity hookup'],
-  tax: '.5',
-  tariffs: {
-    adult: '3.7',
-    child: '2',
-    animal: '1',
-    tent: '4',
-    caravan: '5'
-  },
-  'groupTarrifs': [
-    {
-      description: '1 tente, 2 personnes et 1 voiture avec taxe de séjour',
-      price: '12.5'
-    }
-  ],
-  pitches: {
-    tent: 7,
-    caravan: 3
-  },
-  paymentMethods: {
-    creditCard: true,
-    cash: true
-  },
-  contact: {
-    email: 'andycamping@tent.com',
-    phone: {
-      diallingCode: '+34',
-      phoneNumber: '6354632'
-    }
-  },
-  address: {
-    address: [
-      ''
-    ],
-    gps: {
-      latitude: '12.4325',
-      longitude: '31.3432'
-    }
-  },
-  openTimes: {
-    opens: '31st May',
-    closes: '31st September'
-  },
-  lastUpdated: '12/10/2017'
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -77,7 +28,13 @@ app.get('/find/?country/?region/?name', (req, res) => {
 })
 
 app.get('/location/:latitude/:longitude/:range', (req, res) => {
-  let country = req.params.country;
+  db.collection('campsites').find({ location:
+   { $geoWithin:
+      { $centerSphere: [ [ Number(req.params.latitude), Number(req.params.longitude) ], Number(req.params.range) / 3963.2 ] } } })
+      .toArray((err, results) => {
+        res.send(results);
+      });
+})
 
 app.post('/campsite', (req, res) => {
   // validate req.body-parser?
